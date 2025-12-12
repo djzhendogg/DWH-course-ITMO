f7388a51f36421f3d9ab414ccac5c01350edf8d8
djzhendogg <djzhendogg.is.busy2@gmail.com>
2025-12-12 20:46:16 +0300

    kafka 5

--- kafka_lab/raw_consumer.py
+++ kafka_lab/raw_consumer.py
@@ -1,20 +1,26 @@
 import os
 import sys
 
+from dotenv import load_dotenv
 from confluent_kafka import Consumer, KafkaError
+
+load_dotenv("env.conf")
 
 
 if __name__ == '__main__':
     conf = {
-        'bootstrap.servers': os.getenv('BROKER'),
+        'bootstrap.servers': os.getenv('LAB02_KAFKA_ADDR'),
         'group.id': 'foo',
         'auto.offset.reset': 'smallest',
     }
 
+    print(conf['bootstrap.servers'])
+
     consumer = Consumer(conf)
-    consumer.subscribe([os.getenv('TOPIC')])
+    consumer.subscribe([os.getenv('LAB02_KAFKA_TOPIC')])
 
     try:
+        counter = 0
         while True:
             msg = consumer.poll(timeout=1)
             if msg is None:
@@ -25,6 +31,12 @@ if __name__ == '__main__':
                   f'{msg.key()=}',
                   f'{msg.value().decode()=}',
                 )
+
+
+               counter += 1
+               if counter >= 100:
+                   break
+
             elif msg.error().code() == KafkaError._PARTITION_EOF:
                 # End of partition event
                 info = f'%% {msg.topic()} [{msg.partition()}] reached end at offset {msg.offset()}\n'
