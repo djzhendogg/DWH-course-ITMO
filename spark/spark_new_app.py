bebd4ba38d86ff08012aabbb64c93ac0c86c79e9
djzhendogg <djzhendogg.is.busy2@gmail.com>
2025-12-18 17:24:53 +0300

    spark:48

--- spark/spark_new_app.py
+++ spark/spark_new_app.py
@@ -136,6 +136,7 @@ def six_task(ratings, tags):
     # округлим для аккуратного вывода
     # avg_time_diff = 48201779.226911314
     # avg_time_diff = 48243234.8324159
+    avg_time_diff = round(avg_time_diff)
     logger.info(f"timeDifference:{avg_time_diff}")
     hdfs_append(f"timeDifference:{avg_time_diff}")
 
@@ -187,14 +188,14 @@ def eight_task(ml_df, sc):
     predictions_df = ml_df.withColumn("prediction", predict_udf(col("tag")))
 
     # убедимся, что UDF работает
-    predictions_df.show(50)
+    # predictions_df.show(50)
 
     rmse = (predictions_df.select(((col("prediction") - col("rating")) ** 2).alias("sq_error")).groupBy().avg(
         "sq_error").collect()[0][0])
 
     rmse = float(np.sqrt(rmse))
     rmse = round(rmse, 4)
-
+    logger.info(f"rmse:{rmse}")
     hdfs_append(f"rmse:{rmse}")
 
 
